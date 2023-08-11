9ecdebe2e7d44053cd3a960e1efb3b6312aaa58c
white.fang <nilp71425@gmail.com>
2023-08-11 23:32:24 +0530

    add menu title to side bar

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ with col2:
     st.write("""White Fang, novel by Jack London, published in 1906. The novel was intended as a companion piece to The Call of the Wild (1903), in which a domesticated dog reverts to a wild state. White Fang is the story of a wolf dog that is rescued from its brutal owner and gradually becomes domesticated through the patience and kindness of its new owner, Weedon Scott. White Fang eventually defends Scott’s father from attack by an escaped convict.""")
 
 st.header("whitefang stories")
-st.subheader("Dragon Killer")
+st.subheader("Dragon and Wolfs")
 st.subheader("Lone Wolf")
 st.subheader("Thirsty Beast")
+
+st.sidebar.title("Menu")
