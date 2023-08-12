0015a54659b316eb0e246422e91b7e33796f3d31
white.fang <nilp71425@gmail.com>
2023-08-12 16:29:26 +0530

    add dropdown and button to sidebar

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Whitefang")
 col1,col2=st.columns(2)
 
 with col1:
-    st.image('images.jfif')
+    st.image('images1.jfif')
 with col2:
     st.write("""White Fang, novel by Jack London, published in 1906. The novel was intended as a companion piece to The Call of the Wild (1903), in which a domesticated dog reverts to a wild state. White Fang is the story of a wolf dog that is rescued from its brutal owner and gradually becomes domesticated through the patience and kindness of its new owner, Weedon Scott. White Fang eventually defends Scott’s father from attack by an escaped convict.""")
 
@@ -22,3 +22,7 @@ st.sidebar.markdown("""
 - Login
 - More
 """)
+
+
+st.sidebar.selectbox('Select one',['personal','public'])
+st.sidebar.button('Select')
