461d2eb98b208f30fe2910f046a1e34762532cd7
white.fang <nilp71425@gmail.com>
2023-09-08 10:52:04 +0530

    fang change kiya

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ col1,col2=st.columns(2)
 with col1:
     st.image('images1.jfif')
 with col2:
-    st.write("""White Fang, novel by Jack London, published in 1906. The novel was intended as a companion piece to The Call of the Wild (1903), in which a domesticated dog reverts to a wild state. White Fang is the story of a wolf dog that is rescued from its brutal owner and gradually becomes domesticated through the patience and kindness of its new owner, Weedon Scott. White Fang eventually defends Scott’s father from attack by an escaped convict.""")
+    st.write("""White fang, novel by Jack London, published in 1906. The novel was intended as a companion piece to The Call of the Wild (1903), in which a domesticated dog reverts to a wild state. White Fang is the story of a wolf dog that is rescued from its brutal owner and gradually becomes domesticated through the patience and kindness of its new owner, Weedon Scott. White Fang eventually defends Scott’s father from attack by an escaped convict.""")
 
 st.header("whitefang stories")
 st.subheader("Dragon and Wolfs")
